95c6344411ad72280fe3afc5e94e7c4cb01e293a
Zachary Satoshi Feldcamp <zachary.feldcamp@me.com>
2016-08-11 17:50:55 -0400

    Implement advanced login

---
+++ test/helpers/sessions_helper_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class SessionsHelperTest < ActionView::TestCase
+
+    def setup
+        @user = users(:michael)
+        remember(@user)
+    end
+
+    test "user id exists in cookies" do
+        assert_not_nil cookies.signed[:user_id]
+    end
+
+    test "remember token exists in cookies" do
+        assert_not_nil cookies.signed[:remember_token]
+    end
+
+    test "an unset cookie does not exist in cookies" do
+        assert_nil cookies.signed[:booga]
+    end
+
+    test "current_user returns right user when session is nil" do
+        assert_equal @user, current_user
+        assert is_logged_in?
+    end
+
+    test "current_user returns nil when remember digest is wrong" do
+        @user.update_attribute(:remember_digest, User.digest(User.new_token))
+        assert_nil current_user
+    end
+
+end
